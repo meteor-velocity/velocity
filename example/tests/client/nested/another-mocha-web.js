2a4df311b3ffd4fc09a851de27e0a5d27cbc37dd
rissem <rissem@gmail.com>
2014-05-01 15:57:53 -0700

    for now limit mochaweb to server side js

--- example/tests/client/nested/another-mocha-web.js
+++ example/tests/client/nested/another-mocha-web.js
@@ -1,1 +1,10 @@
 //something here
+var assert = Npm.require("assert");
+
+describe("The Second Group", function(){
+  it("The Second Test", function(){
+    console.log("Second Test Executing");
+    assert(true, "a true test");
+  })
+});
+
