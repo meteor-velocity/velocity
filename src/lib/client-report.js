06f9797f164909c4170673df37c16fd49ff68993
Sam Hatoum <sam@hatoum.net>
2014-05-28 19:17:14 -0700

    accounts-ui conflict bug fix

--- src/lib/client-report.js
+++ src/lib/client-report.js
@@ -1,36 +1,31 @@
-Meteor.startup(function () {
+Template.velocityTestFiles.testFiles = function () {
+  return VelocityTestFiles.find();
+};
 
-    Template.velocityTestFiles.testFiles = function () {
-        return VelocityTestFiles.find();
-    };
+Template.velocityTestReports.testReports = function () {
+  return VelocityTestReports.find();
+};
 
-    Template.velocityTestReports.testReports = function () {
-        return VelocityTestReports.find();
-    };
+Template.velocityAggregateReports.aggregateReports = function () {
+  return VelocityAggregateReports.find();
+};
 
-    Template.velocityAggregateReports.aggregateReports = function () {
-        return VelocityAggregateReports.find();
-    };
+Template.velocityLogs.logs = function () {
+  return VelocityLogs.find();
+};
 
-    Template.velocityLogs.logs = function () {
-        return VelocityLogs.find();
-    };
+Template.velocity.aggregateResult = function () {
+  return VelocityAggregateReports.findOne('result');
+};
 
-    Template.velocity.aggregateResult = function () {
-        return VelocityAggregateReports.findOne('result');
-    };
+Template.velocity.overlayVisibility = function () {
+  return amplify.store('velocityOverlayIsVisible') ? 'block' : 'none';
+};
 
-    Template.velocity.overlayVisibility = function () {
-        return amplify.store('velocityOverlayIsVisible') ? 'block' : 'none';
-    };
-
-    Template.velocity.events({
-        'click #velocity-status-widget': function () {
-            var $overlay = $('#velocity-overlay');
-            $overlay.toggle();
-            amplify.store('velocityOverlayIsVisible', $overlay.is(':visible'));
-        }
-    });
-
-
+Template.velocity.events({
+  'click #velocity-status-widget': function () {
+    var $overlay = $('#velocity-overlay');
+    $overlay.toggle();
+    amplify.store('velocityOverlayIsVisible', $overlay.is(':visible'));
+  }
 });
