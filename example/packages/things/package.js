f9ad0213dfbcaa14ced5e2417cf2a8d221c071ec
alanning <alanning@gmail.com>
2014-06-24 23:42:13 -0400

    use jasmine-unit v0.1.11.  add iron-router.

--- example/packages/things/package.js
+++ example/packages/things/package.js
@@ -6,6 +6,8 @@ Package.on_use(function (api, where) {
   if(api.export) {
     api.export('thing1');
     api.export('thing2');
+    api.export('thing3');
+    api.export('thing4');
   }
   where = where || ['client', 'server'];
   api.add_files('things.js', where);
