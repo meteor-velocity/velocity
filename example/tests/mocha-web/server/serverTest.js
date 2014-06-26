1b3d5be545857e3f235be1444c1a54707ab84a74
rissem <rissem@gmail.com>
2014-06-26 14:57:12 -0700

    make server test more robust

--- example/tests/mocha-web/server/serverTest.js
+++ example/tests/mocha-web/server/serverTest.js
@@ -1,8 +1,8 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
     describe("Server initialization", function(){
-      it("should insert 6 players into the database after server start", function(){
-        chai.assert.equal(Players.find().count(), 6);
+      it("should insert players into the database after server start", function(){
+        chai.assert(Players.find().count() > 0);
       });
     });
   });
