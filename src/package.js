72899cf4dd15805bd3e0a1aa03f74915acec92e0
Sam Hatoum <sam@hatoum.net>
2014-06-27 22:18:14 -0700

    removing lib dir as it only has 2 files

--- src/package.js
+++ src/package.js
@@ -10,13 +10,12 @@ Npm.depends({
 });
 
 Package.on_use(function (api) {
-  api.use(['mirror'], 'server');
 
-  api.add_files('lib/collections.js', ['client', 'server']);
+  api.add_files('collections.js', ['client', 'server']);
   api.export('VelocityTestFiles', ['client', 'server']);
   api.export('VelocityTestReports', ['client', 'server']);
   api.export('VelocityAggregateReports', ['client', 'server']);
   api.export('VelocityLogs', ['client', 'server']);
 
-  api.add_files('lib/main.js', 'server');
+  api.add_files('main.js', 'server');
 });
