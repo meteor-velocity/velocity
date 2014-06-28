d4766b4acc18907bd232de5c11c20366a33b3239
Sam Hatoum <sam@hatoum.net>
2014-06-27 22:17:58 -0700

    removing example from velocity

--- example/tests/custom-stub.js
+++
@@ -1,6 +0,0 @@
-/*
- This file will be loaded by rtd-unit:
-      After the rtd stubs
-      Before any meteor files being loaded
-
-*/
--- example/tests/mocha-web/server/serverTest.js
+++
@@ -1,9 +0,0 @@
-if (!(typeof MochaWeb === 'undefined')){
-  MochaWeb.testOnly(function(){
-    describe("Server initialization", function(){
-      it("should insert players into the database after server start", function(){
-        chai.assert(Players.find().count() > 0);
-      });
-    });
-  });
-}
