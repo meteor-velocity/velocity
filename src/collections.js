bbe87b9d0b0c9ba314ace9b42b63221c51a53517
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-06-30 10:10:01 +0200

    fix: Velocity doesn't work without autopublish (fixes #28)

--- src/collections.js
+++ src/collections.js
@@ -2,3 +2,27 @@ VelocityTestFiles = new Meteor.Collection("velocityTestFiles");
 VelocityTestReports = new Meteor.Collection("velocityTestReports");
 VelocityAggregateReports = new Meteor.Collection("velocityAggregateReports");
 VelocityLogs = new Meteor.Collection("velocityLogs");
+
+if (!Package["autopublish"]) {
+  if (Meteor.isServer) {
+    Meteor.publish("VelocityTestFiles", function () {
+      return VelocityTestFiles.find({});
+    });
+    Meteor.publish("VelocityTestReports", function () {
+      return VelocityTestReports.find({});
+    });
+    Meteor.publish("VelocityAggregateReports", function () {
+      return VelocityAggregateReports.find({});
+    });
+    Meteor.publish("VelocityLogs", function () {
+      return VelocityLogs.find({});
+    });
+  }
+
+  if (Meteor.isClient) {
+    Meteor.subscribe("VelocityTestFiles");
+    Meteor.subscribe("VelocityTestReports");
+    Meteor.subscribe("VelocityAggregateReports");
+    Meteor.subscribe("VelocityLogs");
+  }
+}
