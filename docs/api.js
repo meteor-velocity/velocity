8096e22bb3d420a6099a0a9bb2eade21de229a0a
alanning <alanning@gmail.com>
2014-08-14 16:29:57 -0400

    include API docs

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Velocity"
+    ],
+    "modules": [
+        "Velocity"
+    ],
+    "allModules": [
+        {
+            "displayName": "Velocity",
+            "name": "Velocity"
+        }
+    ]
+} };
+});
