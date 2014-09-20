45314901a2ed744075200d38f0905527e58f261c
Abigail Watson <awatson1978@gmail.com>
2014-09-20 03:37:58 -0400

    validation during postResult is too strict for Nightwatch; relaxing requirements

--- main.js
+++ main.js
@@ -313,6 +313,8 @@ Velocity = {};
      *                               ex. 'Template.leaderboard.selected_name'
      */
     postResult: function (data) {
+      // Nightwatch doesn't return failureType, failureMessage, feailureStackTrace, or ancestors
+      // we can't assume that a test framework will have that informationPhoenix42
       check(data, Match.ObjectIncluding({
         id: String,
         name: String,
@@ -323,11 +325,11 @@ Velocity = {};
         browser: Match.Optional(_matchOneOf(['chrome', 'firefox', 'internet explorer', 'opera', 'safari'])), // TODO: Add missing values
         timestamp: Match.Optional(Match.OneOf(Date, String)),
         async: Match.Optional(Boolean),
-        timeOut: Match.Optional(Match.Any),
-        failureType: Match.Optional(String),
-        failureMessage: Match.Optional(String),
-        failureStackTrace: Match.Optional(Match.Any),
-        ancestors: Match.Optional([String])
+        timeOut: Match.Optional(Match.Any)
+        //failureType: Match.Optional(String),
+        //failureMessage: Match.Optional(String),
+        //failureStackTrace: Match.Optional(Match.Any),
+        //ancestors: Match.Optional([String])
       }));
 
       VelocityTestReports.upsert(data.id, {$set: data});
