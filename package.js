45feaddf584012f143ee105141fc725e03a9b8f8
Abigail Watson <awatson1978@gmail.com>
2014-09-20 20:37:28 -0400

    last of the console logs removed; switching to local development

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 Package.describe({
   name: 'velocity:core',
   summary: 'Velocity, a Meteor specific test-runner',
-  version: '0.2.11,
+  version: '0.2.11',
   git: 'https://github.com/xolvio/velocity.git'
 });
 
