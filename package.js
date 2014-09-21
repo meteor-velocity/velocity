1a785b625789822f2801a5b022f728972de05597
Abigail Watson <awatson1978@gmail.com>
2014-09-20 20:12:58 -0400

    backing out the rest of the console logging again

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 Package.describe({
   name: 'velocity:core',
   summary: 'Velocity, a Meteor specific test-runner',
-  version: '0.2.9',
+  version: '0.2.10',
   git: 'https://github.com/xolvio/velocity.git'
 });
 
