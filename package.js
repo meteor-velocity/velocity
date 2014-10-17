78c370986dadc41557b40af59e2a25c06ddd71e3
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-10-17 14:32:51 +0200

    Bumps version to 0.2.15

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 Package.describe({
   name: 'velocity:core',
   summary: 'Velocity, a Meteor specific test-runner',
-  version: '0.2.14',
+  version: '0.2.15',
   git: 'https://github.com/xolvio/velocity.git'
 });
 
