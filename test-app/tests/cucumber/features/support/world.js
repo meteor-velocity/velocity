a432ca83dfb7d1de626acdb61922fb8a1e551836
sam@hatoum.net <sam@hatoum.net>
2015-04-01 09:16:49 -0700

    debugging - feature files might help!

---
+++ test-app/tests/cucumber/features/support/world.js
@@ -0,0 +1,27 @@
+(function () {
+
+  'use strict';
+
+  module.exports = function () {
+
+    var helper = this;
+    var os = require('os');
+
+    this.World = function (next) {
+
+      helper.world = this;
+
+      helper.world.cwd = os.tmpdir();
+
+      helper.world.cucumber = Package['xolvio:cucumber'].cucumber;
+
+      Package['xolvio:webdriver'].wdio.getGhostDriver(function (browser) {
+        helper.world.browser = browser;
+        browser.call(next);
+      });
+
+    };
+
+  };
+
+})();
