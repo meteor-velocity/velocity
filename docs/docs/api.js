af8025f602c2f6e92867fd10aa039e7d41f4f608
Velocity Robot <robot@meteorvelocity.com>
2015-04-09 15:03:09 -0700

    Update docs

---
+++ docs/docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Meteor.methods",
+        "Velocity"
+    ],
+    "modules": [
+        "Velocity"
+    ],
+    "allModules": [
+        {
+            "displayName": "Velocity",
+            "name": "Velocity"
+        }
+    ]
+} };
+});
