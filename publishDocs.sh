99b57323820ce2f38d6196a99506b606ce030fac
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-11 01:08:00 +0200

    Automation for publishing docs
    Based on Mikes work.

---
+++ publishDocs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -u
+
+git config --global user.email "velocity-robot@xolv.io"
+git config --global user.name "Velocity Robot"
+
+rm -rf .build || true
+rm -rf docs || true
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+yuidoc .
+mv docs /tmp/docs
+git remote set-branches --add origin gh-pages
+git fetch
+git checkout gh-pages
+# Remove everything but .git
+rm -r *
+mv /tmp/docs/* ./
+git add --all .
+git commit --message "Update docs [skip ci]"
+git push --force origin gh-pages
+git checkout $branch
