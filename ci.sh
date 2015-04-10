3add114fea1c02bc26081521172d1f9a7efb648d
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-04-10 17:02:23 +0200

    Adds `yuidoc --lint` to CI script
    Also did some reordering of steps, so that
    JSHint and Yuidoc is the first thing that is checked.

--- ci.sh
+++ ci.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+# Run this script from the root folder of the repository with:
+# ./ci.sh
+
 # Exit on first command that fails
 set -e
-
-# Run this script from the root folder of the repository with:
-# ./test.sh
-
-echo "Killing node and phantomjs processes"
-pkill -9 phantomjs || true
-pkill -9 node || true
-rm -rf test-app/packages/tests-proxy
-
 
 echo "Checking JSHint"
 jshint .
 echo "JSHint checks were successful"
 
+# Make sure our documentation is clean
+echo "Checking Yuidoc syntax"
+yuidoc --lint
+echo "Yuidoc checks were successful"
+
 echo "Testing app"
+
+echo "Killing node and phantomjs processes"
+pkill -9 phantomjs || true
+pkill -9 node || true
+rm -rf test-app/packages/tests-proxy
 
 cd test-app
 rm -rf .meteor/local
@@ -31,4 +35,4 @@ export JASMINE_SERVER_UNIT=0
 export JASMINE_SERVER_INTEGRATION=1
 #export VELOCITY_DEBUG=1
 
-meteor --test --once
+meteor --test
