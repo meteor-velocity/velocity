261bbfcf668d2a3bc94c5ef0bdeb9daabe83db40
Velocity Robot <velocity-robot@xolv.io>
2015-04-10 22:49:20 +0000

    Update docs [skip ci]

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Collections",
+        "Meteor.methods",
+        "Velocity"
+    ],
+    "modules": [
+        "Velocity"
+    ],
+    "allModules": [
+        {
+            "displayName": "Velocity",
+            "name": "Velocity"
+        }
+    ]
+} };
+});
