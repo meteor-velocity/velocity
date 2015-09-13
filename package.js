db3178db564d30c90a6f298f21a4e5d7666255aa
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-13 14:42:44 +0200

    Release of 0.10.0-rc.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
   name: 'velocity:core',
   summary: 'Velocity, a Meteor specific test-runner',
-  version: '0.10.0-rc.1',
+  version: '0.10.0-rc.2',
   git: 'https://github.com/meteor-velocity/velocity.git',
   debugOnly: true
 });
