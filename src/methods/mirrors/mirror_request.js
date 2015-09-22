4d39c605c332280b3ff45c4951c424de457335ae
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-22 13:15:48 +0200

    Use velocity:METEOR@1.2.0.1

--- src/methods/mirrors/mirror_request.js
+++ src/methods/mirrors/mirror_request.js
@@ -13,11 +13,11 @@ Npm.require('colors');
 
 // Specifies the Meteor release that we use for mirrors
 Velocity.mirrorMeteorReleaseName = 'velocity:METEOR';
-Velocity.mirrorMeteorVersion = '1.2-rc.15_2';
+Velocity.mirrorMeteorVersion = '1.2.0.1_1';
 Velocity.mirrorMeteorRelease =
   Velocity.mirrorMeteorReleaseName + '@' + Velocity.mirrorMeteorVersion;
 Velocity.mirrorMeteorToolReleaseName = 'velocity:meteor-tool';
-Velocity.mirrorMeteorToolVersion = '1.1.7-rc.14_3';
+Velocity.mirrorMeteorToolVersion = '1.1.8_1';
 Velocity.mirrorMeteorToolRelease =
   Velocity.mirrorMeteorToolReleaseName + '@' + Velocity.mirrorMeteorToolVersion;
 
