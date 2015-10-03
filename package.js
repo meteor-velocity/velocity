8872f0d6ddf37ecdea3210ed90bffd2c8046f8e6
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-10-03 22:34:52 +0200

    Updates chokidar

--- package.js
+++ package.js
@@ -30,7 +30,7 @@ Package.on_use(function (api) {
   api.use('check');
   api.use('random');
   api.use('underscore'); // Used for the Function.bind polyfill
-  api.use('velocity:chokidar@1.0.3_1', 'server');
+  api.use('velocity:chokidar@1.2.0_1', 'server');
   api.use('velocity:meteor-internals@1.1.0_7');
   api.use('velocity:source-map-support@0.3.2_1');
   api.use('sanjo:long-running-child-process@1.1.3', 'server');
