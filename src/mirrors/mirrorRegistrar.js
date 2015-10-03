bf55893c409ad2fb5c52f6d24ed64abf9c489f61
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-10-03 22:40:05 +0200

    Improves handshake logic

--- src/mirrors/mirrorRegistrar.js
+++ src/mirrors/mirrorRegistrar.js
@@ -16,29 +16,30 @@ DEBUG = !!process.env.VELOCITY_DEBUG;
   if (process.env.IS_MIRROR) {
     if (process.env.HANDSHAKE) {
       WebApp.onListening(function () {
-        DEBUG && console.log('[velocity] Mirror started. Connecting via DDP to parent');
-
-        var velocityConnection = DDP.connect(process.env.PARENT_URL);
-        velocityConnection.onReconnect = function () {
-          DEBUG && console.log('[velocity] Mirror connected to parent. Registering mirror...');
-          velocityConnection.call('velocity/mirrors/register', {
-            framework: process.env.FRAMEWORK,
-            host: process.env.HOST,
-            port: process.env.MIRROR_PORT
-          }, function (error) {
-            if (error) {
-              console.error(
-                '[velocity] Could not connect to parent via DDP. ' +
-                'Please restart your app and try again. ' +
-                'If this happens often please report it as issue to velocity:core.',
-                error
-              );
+        Meteor.defer(function handshakeWithParent() {
+          DEBUG && console.log('[velocity] Mirror started. Connecting via DDP to parent');
+          var velocityConnection = DDP.connect(process.env.PARENT_URL, {
+            onConnected: function () {
+              DEBUG && console.log('[velocity] Mirror connected to parent. Registering mirror...');
+              velocityConnection.call('velocity/mirrors/register', {
+                framework: process.env.FRAMEWORK,
+                host: process.env.HOST,
+                port: process.env.MIRROR_PORT
+              }, function (error) {
+                if (error) {
+                  console.error(
+                    '[velocity] Could not connect to parent via DDP. ' +
+                    'Please restart your app and try again. ' +
+                    'If this happens often please report it as issue to velocity:core.',
+                    error
+                  );
+                }
+                // Disconnect because we no longer need the connection
+                velocityConnection.disconnect();
+              });
             }
-            // Disconnect because we no longer need the connection
-            velocityConnection.disconnect();
           });
-        };
-
+        });
       });
     } else {
       DEBUG && console.log('[velocity] Mirror', process.env.MIRROR_PORT , 'configured not to handshake');
