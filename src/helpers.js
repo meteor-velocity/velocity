4c7546392a99b88b136d10932185e4f685c2d857
Sam Hatoum <sam@xolv.io>
2015-10-06 11:52:41 -0700

    Starts mirror with --once in CI
    Also extracts a common method for parsing environment boolean variables

--- src/helpers.js
+++ src/helpers.js
@@ -12,3 +12,20 @@ VelocityInternals.isDirectory = function (path) {
   var stat = files.statOrNull(path);
   return stat && stat.isDirectory();
 };
+
+VelocityInternals.isEnvironmentVariableTrue = function (environmentVariable, defaultValue = true) {
+  var type = typeof environmentVariable;
+
+  switch (type) {
+    case 'undefined':
+      return defaultValue;
+    case 'string':
+      if (environmentVariable.toLowerCase() === 'false' ||
+        parseInt(environmentVariable) === 0) {
+        return false;
+      }
+      return true;
+    default:
+      return !!environmentVariable;
+  }
+};
